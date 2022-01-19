0ad2d15d0501ccedc710484a7dd944b47e6dcfeb
Eric Rodrigues <er601432@gmail.com>
2022-01-19 16:01:08 -0300

    fix: adjust in migration of address

--- src/shared/infra/typeorm/migrations/1642615189424-CreateTableAddress.ts
+++ src/shared/infra/typeorm/migrations/1642615189424-CreateTableAddress.ts
@@ -2,7 +2,6 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateTableAddress1642615189424 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'address',
