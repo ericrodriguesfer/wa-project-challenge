88c89d1ac7592c7ee3e69505ed6f6f757024f615
Eric Rodrigues <er601432@gmail.com>
2022-01-22 01:29:25 -0300

    feat: concluding refactor in documentation of swagger

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import IReponseDefaultRoute from '../contract/IResponseDefaultRoute';
 import { AppService } from '../services/app.service';
 
+@ApiTags('wellcome')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'this is default route api, destined the a message of wellcome',
+  })
+  @ApiOkResponse({
+    description: 'This api is working normally, welcome and explore',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error intern in server, please try again',
+  })
   getHello(): IReponseDefaultRoute {
     return this.appService.getHello();
   }
--- src/modules/category/infra/http/category.controller.ts
+++ src/modules/category/infra/http/category.controller.ts
@@ -11,6 +11,14 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import {
+  ApiConflictResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import CreateCategoryDTO from '../../dto/CreateCategoryDTO';
 import UpdateCategoryDTO from '../../dto/UpdateCategoryDTO';
 import CreateCategoryService from '../../services/createCategory.service';
@@ -19,6 +27,7 @@ import ListCategoryService from '../../services/listCategory.service';
 import UpdateCategoryService from '../../services/updateCategory.service';
 import Category from '../typeorm/entities/Category';
 
+@ApiTags('category')
 @Controller('category')
 class CategoryController {
   constructor(
@@ -31,6 +40,16 @@ class CategoryController {
   @Get()
   @UsePipes(ValidationPipe)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({
+    summary: 'this route list all categorys registereds in our database',
+  })
+  @ApiOkResponse({
+    description: 'Listing all categorys registereds in our database',
+    type: Category,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error intern in server, please try again',
+  })
   getCategorys(): Promise<Array<Category>> {
     return this.listCategoryService.execute();
   }
@@ -38,6 +57,19 @@ class CategoryController {
   @Post()
   @UsePipes(ValidationPipe)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({
+    summary: 'this route create a category in our database',
+  })
+  @ApiOkResponse({
+    description: 'Creating the category in our database',
+    type: Category,
+  })
+  @ApiConflictResponse({
+    description: 'This name repassed with category already exists',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error intern in server, please try again',
+  })
   createCategory(@Body() { name }: CreateCategoryDTO): Promise<Category> {
     return this.createCategoryService.execute({ name });
   }
@@ -45,6 +77,22 @@ class CategoryController {
   @Put(':id')
   @UsePipes(ValidationPipe)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({
+    summary: 'this route update a category of our database',
+  })
+  @ApiOkResponse({
+    description: 'Updateing the category in our database',
+    type: Category,
+  })
+  @ApiNotFoundResponse({
+    description: 'Not possible found this category in our database',
+  })
+  @ApiConflictResponse({
+    description: 'This name repassed with category already exists',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error intern in server, please try again',
+  })
   updateCategory(
     @Param('id') id: number,
     @Body() { name }: UpdateCategoryDTO,
@@ -55,6 +103,18 @@ class CategoryController {
   @Delete(':id')
   @UsePipes(ValidationPipe)
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({
+    summary: 'this route delete a category of our database',
+  })
+  @ApiOkResponse({
+    description: 'Deleting this category in our database',
+  })
+  @ApiNotFoundResponse({
+    description: 'Not possible found this category in our database',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Error intern in server, please try again',
+  })
   deleteCategory(@Param('id') id: number): Promise<boolean> {
     return this.deleteCategoryService.execute(id);
   }
